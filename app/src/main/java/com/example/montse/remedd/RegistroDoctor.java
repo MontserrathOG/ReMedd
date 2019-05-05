23eb451d64a6af66dd66da7fed53ea8507632b08
MontserrathOG <lightmon16@gmail.com>
2019-05-04 19:02:32 -0500

    registro doctor

---
+++ app/src/main/java/com/example/montse/remedd/RegistroDoctor.java
@@ -0,0 +1,13 @@
+package com.example.montse.remedd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistroDoctor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_doctor);
+    }
+}
